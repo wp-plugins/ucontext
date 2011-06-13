9209169f583415e2cbcaa38640234993cdcfe9cb
gcornelisse <gcornelisse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-13 14:56:15 +0000

    check arrays before foreach
    git-svn-id: https://plugins.svn.wordpress.org/ucontext/trunk@396706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ucontext_library.php
+++ ucontext_library.php
@@ -1,25 +1,32 @@
 <?php
-/**
- * @Copyright Copyright (C) 2010 Summit Media Concepts LLC
- * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
- */
-
-/**
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+/*
+Plugin Name: Clickbank In-Text Affiliate Links
+Plugin URI: http://www.uContext.com
+Description: Automatically finds keyword phrases and converts them into contextual in-text Clickbank affiliate links.
+Author: Summit Media Concepts LLC
+Author URI: http://www.SummitMediaConcepts.com
+Tags: clickbank, affiliate, links, ads, advertising, post, context, contextual
+Version: 1.5
 */
+
+/**
+
+	Copyright 2010  Summit Media Concepts LLC (email : info@SummitMediaConcepts.com)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
 
 class uContext
 {
